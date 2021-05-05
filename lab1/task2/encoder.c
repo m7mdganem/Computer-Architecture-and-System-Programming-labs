ab0b2dd1d8ea557f274d2ea49536e70329aa54f4
Mohammad Ghanem <80080334+m7mdganem@users.noreply.github.com>
2021-05-05 08:09:13 +0300

    Add files via upload

---
+++ lab1/task2/encoder.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <string.h>
+typedef enum {false=0, true=1} bool;
+
+int getEchar(const char c){
+    if(c >= 48 & c <= 57) return c-'0';
+    else if(c >= 65 & c <= 70) return c-55;
+    return -1;
+}
+
+int main(int argc, char const *argv[]){
+    
+    //updating program configuration
+    bool debugMode = false;
+    bool encryptMode = false;
+    bool increase = false; //indicating if the encryption is with + or -
+    int eChar = 0; //indicating the encryption key
+    FILE *inputStream = stdin;
+    FILE *outputStream = stdout;
+    for(int i=1; i<argc; i++){
+        if(strncmp(argv[i],"-D",2)==0) debugMode = true;
+        else if (strncmp(argv[i],"+e",2)==0){
+            encryptMode = true; 
+            increase = true; 
+            eChar = getEchar(argv[i][2]);
+            if(eChar == -1){//if the given key is outside the range print error message and exit
+                fprintf(stderr,"The encryption key shold be btween 0-F\n");
+                return 1;
+            }
+        }
+        else if (strncmp(argv[i],"-e",2)==0){
+            encryptMode = true; 
+            eChar = getEchar(argv[i][2]);
+            if(eChar == -1){//if the given key is outside the range print error message and exit
+                fprintf(stderr,"The encryption key shold be btween 0-F\n");
+                return 1;
+            }
+        }
+        else if (strncmp(argv[i],"-i",2)==0){
+            inputStream = fopen(argv[i]+2,"r");
+            if(inputStream == NULL){fprintf(stderr,"Error reading input file"); return 1;}
+        }
+        else if (strncmp(argv[i],"-o",2)==0){
+            outputStream = fopen(argv[i]+2,"w");
+        }
+        
+    }
+	if(debugMode) fprintf(stderr,"-D\n");
+
+    //Run the encoder
+    int ch;
+    int counter = 0;
+    while( (ch = fgetc(inputStream)) != EOF){
+        //if in debugging mode print the value of the char before midifying
+        if(debugMode & ch != '\n') fprintf(stderr,"%d    ",ch);
+        
+        //encode to lower case or encrypt
+        if(!encryptMode){
+            if (ch <= 90 & ch >= 65){ch += 32; counter++;}
+        }else if(ch != '\n'){
+            if(increase){ ch += eChar; counter++;}//in case of +e
+            else{ch -= eChar; counter++;}//in case of -e
+        }
+
+        //if in debugging mode print the value of the char after midifying
+        if(debugMode & ch != '\n') fprintf(stderr,"%d\n",ch);
+
+        //if in debugging mode and finished a line : print num of modified chars and reset counter
+        if(debugMode & ch == '\n'){
+            fprintf(stderr,"the number of letters: %d\n",counter);
+            counter = 0;
+        }
+
+        fputc(ch,outputStream); //print the modified char
+    }
+
+    if(debugMode & ch == EOF & counter > 0){
+            fprintf(stderr,"the number of letters: %d\n",counter);
+            counter = 0;
+            fputc('\n',outputStream);
+    }
+
+    return 0;
+}
